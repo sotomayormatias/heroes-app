a4bdd82465adf1c1fd29875173217d4c49fe2782
Matias Sotomayor <matisoto_77@hotmail.com>
2025-03-18 01:22:14 -0300

    add routes and heroes card and list

---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,12 @@
+import { HeroesList } from "../components"
+
+export const DcPage = () => {
+  return (
+    <>
+      <h1>DC Heroes</h1>
+      <hr />
+
+      <HeroesList publisher='DC Comics' />
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import './styles.css'
 import { HeroesApp } from './HeroesApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HeroesApp />
+    <BrowserRouter>
+      <HeroesApp />
+    </BrowserRouter>
   </StrictMode>,
 )
